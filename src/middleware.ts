841e8ae069b0560a387f8650510bc1aa96a821b1
bhowmik1234 <124621278+bhowmik1234@users.noreply.github.com>
2024-10-26 23:42:49 +0530

    adde sign

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,8 +7,9 @@ export async function middleware(req: NextRequest) {
   const protectedRoutes = ['/discover', '/finder'];
 
   if (!token && protectedRoutes.includes(pathname)) {
-    return NextResponse.redirect("http://localhost:3000/api/auth/signin");
+    return NextResponse.redirect("https://direct-reach-ai-tmey.vercel.app/api/auth/signin?callbackUrl=https%3A%2F%2Fdirect-reach-ai-tmey.vercel.app%2F");
   }
+//   https://direct-reach-ai-tmey.vercel.app/api/auth/signin?callbackUrl=https%3A%2F%2Fdirect-reach-ai-tmey.vercel.app%2F
 
   return NextResponse.next();
 }
